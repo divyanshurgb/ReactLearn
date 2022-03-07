4dbf849b3d4deb4ac2ccaf0277d573e02f518724
Divyanshu Singh <divic49@gmail.com>
2022-03-07 18:06:16 +0530

    halfDROPDOWN

--- src/Components/Dropdown/DropdownData.js
+++ src/Components/Dropdown/DropdownData.js
@@ -1,24 +1,36 @@
-import React,{ useState } from 'react';
-import { Select } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Menu } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
+import { Button } from 'semantic-ui-react';
 
 const DropdownData = () => {
     
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [value, setValue] = useState(0);
+
+    const handleOpenMenu = e => {
+        setAnchorEl(e.currentTarget);
+    }
+
     const postsdata = [
-        {
+        {   
+            "id" : "1",
             "name": "Garvit",
             "address": "Patelnagar",
             "course": "Btech",
             "branch": "cse",
         },
-        {
+        {   
+            "id" : "2",
             "name": "Gautam",
             "address": "Patelnagar",
             "course": "Btech",
             "branch": "ece",
         },
-        {
+        {    
+            "id" : "3",
             "name": "Shivam",
             "address": "Rajnagar",
             "course": "MBA",
@@ -28,28 +40,54 @@ const DropdownData = () => {
     
     const [posts, setpost] = useState(postsdata);
 
-    const handleChange = (e) => {
-        let varpost1 = e.target.value
-        console.log(varpost1)
-    }
-    
-    console.log(postsdata)
-    let varpost = postsdata.filter(post => post.userId == '1')
-
-    console.log(varpost);
-
     return (
     <div> 
+        <div>
         {
-        postsdata.map((post) => <li> {post} </li> ) 
+        posts.map((post) => <div key = {post.id}> <li> {post.name} </li>  </div>) 
         } 
-        
-        {/* <Select className="width50" value={this.state.selected} onChange={this.handleChange}>
-            {this.renderOptions()}
-        </Select> */}
+        </div>
+        <div> 
+        <Button 
+        aria-controls = 'menu'
+        onMouseOver = {handleOpenMenu}
+        disableRipple
+        // className = {classes.accountButton}
+        variant = 'contained'
+        color = 'secondary'>
+        Profile
+        </Button> 
+        <Menu id = 'menu'>
+            <MenuItem>
+               My Account
+            </MenuItem>
+        </Menu>
+        </div>
     </div>
     )
 }
 
-
 export default DropdownData;
+
+
+
+
+
+// Filter
+
+// const handleChange = (e) => {
+    //     let varpost1 = e.target.value
+    //     console.log(varpost1)
+    // }
+    
+    // console.log(postsdata)
+    // let varpost = postsdata.filter(post => post.userId == '1')
+
+    // console.log(varpost);
+
+// Old Select
+
+{/* <Select className="width50" value={this.state.selected} onChange={this.handleChange}>
+            {this.renderOptions()}
+</Select> 
+    */}
